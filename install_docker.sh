8bdaf1cd9bb50bf69bb19bc6eb2f546856a49fda
ashutoshsingh101 <ashsingh6060@gmail.com>
2019-03-07 00:53:02 +0530

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -1,3 +1,13 @@
 sudo apt-get update
-sudo apt-get install docker
-sudo apt-get install docker-compose
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+
+# Install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo docker-compose --version
